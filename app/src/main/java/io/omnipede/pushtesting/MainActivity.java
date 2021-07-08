02b170334120e4cabb7110be93dadaee4169012a
서현규 <omnipede@coinplug.com>
2021-07-08 16:17:50 +0900

    기타: 소스코드 업로드

---
+++ app/src/main/java/io/omnipede/pushtesting/MainActivity.java
@@ -0,0 +1,39 @@
+package io.omnipede.pushtesting;
+
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button logTokenButton = findViewById(R.id.logTokenButton);
+
+        logTokenButton.setOnClickListener(v -> {
+            FirebaseMessaging.getInstance().getToken()
+                    .addOnCompleteListener(task -> {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+                        String msg = getString(R.string.msg_token_fmt, token);
+                        Log.d(TAG, msg);
+                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    });
+        });
+    }
+}
---
+++ app/src/main/java/io/omnipede/pushtesting/MyFirebaseMessagingService.java
@@ -0,0 +1,6 @@
+package io.omnipede.pushtesting;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+}
